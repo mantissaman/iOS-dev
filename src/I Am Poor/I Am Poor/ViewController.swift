d6fff3259f4c476ba6d42e5f870b1812ade527d8
Atul Sharma <asharma@sw-at.com>
2020-05-31 03:26:43 +0100

    all cool stuff

---
+++ src/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Atul Sharma on 31/05/2020.
+//  Copyright © 2020 Atul Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
